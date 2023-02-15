ddbcc92562f2ad7224b5494425db68e9b35c0d88
Hafuketo <haafket@gmail.com>
2023-02-15 18:01:44 +0100

    lagt till en basic testgrej för att kontrollera att det är nummer som matas in

--- BankomatApp/App/BankomatApp.cs
+++ BankomatApp/App/BankomatApp.cs
@@ -8,6 +8,13 @@ namespace BankomatApp.App
         static void Main(string[] args)
         {
             AppScreen.Welcome();
+            // string cardNumber = Utility.GetUserInput("your card numer");
+            long cardNumber = Validator.Convert<long>("Your card num");
+
+            Console.WriteLine($"Your card number is {cardNumber}");
+
+            Utility.PressEnterToContinue();
+
         }
     }
 }
--- BankomatApp/UI/Utility.cs
+++ BankomatApp/UI/Utility.cs
@@ -8,6 +8,12 @@ namespace BankomatApp.UI
 {
     public static class Utility
     {
+        public static string GetUserInput(string prompt)
+        {
+            Console.WriteLine($"Enter {prompt}");
+            return Console.ReadLine();
+        }
+
         public static void PressEnterToContinue()
         {
             Console.WriteLine("\n\nTryck på en aknapp för att fortsätta...\n");
